c67f57466a7fd14cd8ab447492f521a0c9f4fbee
Redete Bezabeh <134902499+redeteb@users.noreply.github.com>
2024-08-13 17:26:23 -0500

    Create dircheck.sh

---
+++ dircheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Script that checks if a directory named backup exists in the current directory. If it does not exist, create it.
+
+if
+
+[ ! -d "$dldir" ] && mkdir -p "$dldir"
+
+test -d /path/to/directory; then
+  echo "Directory exists."
+fi
